4a1010b2e79820e5678374a9d0e235aee885ecb6
Alexander Gavrilov <108229249+AlexandrReactJs@users.noreply.github.com>
2023-08-17 20:12:13 +0300

    modification

--- src/components/ChunkWeather/OneChunk/OneChunk.jsx
+++ src/components/ChunkWeather/OneChunk/OneChunk.jsx
@@ -4,13 +4,17 @@ import styles from './OneChunk.module.css'
 const OneChunk = ({temp, date, icon, posX}) => {
     let degreeCelsiusTemp = Math.floor(temp - 273);
     let arr = date.split(' ')
+    let doneDate  = arr[0].split('-').reverse().join('.')
+    let time = arr[1].split(':')
+    time.pop()
+    let doneTime = time.join(':')
     return (
     <div className={styles.chunk} style={{transform: `translate(${posX}px, 0)`}}>
         <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}  />
         <h3>{degreeCelsiusTemp} °</h3>
         <div className={styles.date}>
-          <p className={styles.time}>{arr[1]}</p>
-          <p className={styles.dateDay}>{arr[0]}</p>
+          <p className={styles.time}>{doneTime}</p>
+          <p className={styles.dateDay}>{doneDate}</p>
         </div>
     </div>
   )
